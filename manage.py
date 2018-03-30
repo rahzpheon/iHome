1c54ba05a17073c56670661a38264d6ec78c02a2
xwp <xwp_fullstack@163.com>
2018-03-30 14:53:01 +0800

    a outstanding upgrade.

--- manage.py
+++ manage.py
@@ -2,11 +2,17 @@
 
 from flask import Flask
 
+class Config(object):
+
+    DEBUG = True
+
 app = Flask(__name__)
+app.config.from_object(Config)
+
 
 @app.route('/')
 def foo():
-    return 'Hello Flask!'
+    return 'Hello Flask666!'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
