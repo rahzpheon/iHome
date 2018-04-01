021510b677ad5ec3dda2e24ec19c421dd324f4f1
rahzpheon <rahzpheon@163.com>
2018-04-01 20:30:51 +0800

    图片验证码3:生成的uuid标示用户浏览器,并与验证码值一同存入redis中.在存入新内容时,之前的内容先进行删除.

--- iHome/api_1_0/verify.py
+++ iHome/api_1_0/verify.py
@@ -13,12 +13,25 @@ def get_image_code():
     # 获取url中的uuid
     uuid = request.args.get('uuid')
 
+    # 获取url中的last_uuid
+    last_uuid = request.args.get('last_uuid')
+
     # 校验
     if not uuid:
-        abort(403)
+        # abort(403)
+        return jsonify(errno=RET.NODATA, errmsg=u'uuid不存在')
 
     # 生成图片验证码
     name, text, image = captcha.generate_captcha()
+
+    # 如果存在旧uuid,删除对应数据
+    if last_uuid:
+        try:
+            redis_store.delete('ImageCode' + last_uuid)
+
+        except Exception as e:
+            print e
+            return jsonify(errno=RET.DBERR, errmsg=u'删除旧验证码失败')
 
     # 使用uuid标识验证码txt,uuid代表用户浏览器即其身份,并设置过期事件:300s
     try:
